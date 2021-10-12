dc567ff12c0f3faddd9d2cd58a9801656efe0351
Toby Smith <tobysmith568@hotmail.co.uk>
2021-10-12 23:40:23 +0100

    Bump the Android version

--- app/build/generated/source/buildConfig/release/uk/tobysmith/totp/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/uk/tobysmith/totp/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "uk.tobysmith.totp";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "2";
 }
