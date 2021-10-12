9e3119f113d0b6efabeed105916d8d4e4e64812e
Toby Smith <tobysmith568@hotmail.co.uk>
2021-10-12 23:22:54 +0100

    Add an Android project with Bubblewrap

---
+++ app/src/main/java/uk/tobysmith/totp/DelegationService.java
@@ -0,0 +1,14 @@
+package uk.tobysmith.totp;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
